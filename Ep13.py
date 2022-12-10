5cfdebf4f31d5a43fc6d2a3f6f3daf475ff2cafd
sanamo <kartersanamo05@gmail.com>
2022-12-09 19:28:37 -0600

    Add files via upload
    In this discord.py bot development tutorial, you will learn how to utilize the Cogs feature of the discord.py library. By the end of this tutorial, you will have an understanding of how to use Cogs to better organize and lay out your code to prevent confusion for yourself or other readers.

---
+++ Ep13.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+from colorama import Back, Fore, Style
+import time
+import json
+import platform
+
+class Client(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix=commands.when_mentioned_or('.'), intents=discord.Intents().all())
+
+        self.cogslist = ["cogs.cog1", "cogs.cog2", "cogs.cog3"]
+
+    async def setup_hook(self):
+      for ext in self.cogslist:
+        await self.load_extension(ext)
+
+    async def on_ready(self):
+        prfx = (Back.BLACK + Fore.GREEN + time.strftime("%H:%M:%S UTC", time.gmtime()) + Back.RESET + Fore.WHITE + Style.BRIGHT)
+        print(prfx + " Logged in as " + Fore.YELLOW + self.user.name)
+        print(prfx + " Bot ID " + Fore.YELLOW + str(self.user.id))
+        print(prfx + " Discord Version " + Fore.YELLOW + discord.__version__)
+        print(prfx + " Python Version " + Fore.YELLOW + str(platform.python_version()))
+        synced = await self.tree.sync()
+        print(prfx + " Slash CMDs Synced " + Fore.YELLOW + str(len(synced)) + " Commands")
+
+with open('config.json', 'r') as f:
+    data = json.load(f)
+    TOKEN = data['TOKEN']
+
+client = Client()
+
+client.run(TOKEN)
