f596eeb374c2982cf526e2df90c5593574a1fb67
sanamo <kartersanamo05@gmail.com>
2023-11-02 20:19:58 -0500

    Update tickets.py

--- Advanced Ticket System/Cogs/tickets.py
+++ Advanced Ticket System/Cogs/tickets.py
@@ -1,108 +1,105 @@
 from discord import app_commands
 from discord.ext import commands
 from discord import ui
-from discord.interactions import Interaction
-from discord.ui import select
+import datetime
 import discord
 
 class tickets(commands.Cog):
-  def __init__(self, client: commands.Bot):
-    self.client = client
+    def __init__(self, client: commands.Bot):
+        self.client = client
 
-  @app_commands.command(name="sendtickets", description="Sends the ticket prompt message")
-  async def sendtickets(self, interaction: discord.Interaction):
-    embed = discord.Embed(color=discord.Color.greyple(), description="Please select your ticket category down below that best suits your needs. If you don't see a ticket category that fits your needs, then please select General Support. Our staff team will be with your shortly. Thank you!")
-    await interaction.response.send_message("Successfully sent ticket message!", ephemeral=True)
-    await interaction.channel.send(embed=embed, view=DropDown())
+    @app_commands.command(name="sendtickets", description="Sends the ticket prompt message")
+    async def sendtickets(self, interaction: discord.Interaction):
+        embed = discord.Embed(color=discord.Color.greyple(), description="Please select your ticket category down below. Thank you!")
+        await interaction.response.send_message("Successfully sent ticket message!", ephemeral=True)
+        await interaction.channel.send(embed=embed, view=DropDown())
 
 class DropDown(discord.ui.View):
-  def __init__(self) -> None:
-    super().__init__(timeout=None)
+    def __init__(self) -> None:
+        super().__init__(timeout=None)
   
-  async def create_ticket(self, interaction: discord.Interaction, category: str):
-    staff = discord.utils.get(interaction.guild.roles, name="Staff Team")
-    ticket_descriptions = {
-      "General Support": {"description": "Please be as descriptive as possible with your issues."},
-       "Bug Report": {"description": "Depending on the severity of the bug, you may be eligable for a reward."},
-       "Staff Application": {"description": "**Staff Requirements:**\n- Requirement 1...\n- Requirement 2...\n-Requirement 3..."}
-    }
-    description = ticket_descriptions.get(category, {"description": ""})
-    category = discord.utils.get(interaction.guild.categories, name=category)
-    overwrites = {
-      interaction.guild.default_role: discord.PermissionOverwrite(view_channel=False),
-      interaction.user: discord.PermissionOverwrite(view_channel=True, send_messages=False),
-      staff: discord.PermissionOverwrite(view_channel=True, send_messages=True)
-    }
-    ticket_number = "0001"
-    ticket = await interaction.guild.create_text_channel(name=f"{interaction.user.name}-{ticket_number}", overwrites=overwrites, category=category)
-    tag = await ticket.send(staff.mention)
-    await tag.delete()
-    embed = discord.Embed(description=f"Success! Your ticket has been created {ticket.mention}!", color=discord.Color.greyple())
-    await interaction.followup.send(embed=embed, ephemeral=True)
-    desc = f"Hey {interaction.user.mention}\n \nYou have made a new ticket!\n**Type:** {category.name}\n \n{description['description']}\n \n**One of our staff members will be with you shortly.**"
-    ticket_embed = discord.Embed(description=desc, color=discord.Color.greyple())
-    await ticket.send(embed=ticket_embed, view=EnterInfo(category))
+    async def create_ticket(self, interaction: discord.Interaction, selection: str):
+        # Get the Ticket "Description"
+        ticket_descriptions = {"General Support": "Our staff team will be with you shortly!",
+                               "Bug Support": "Please do not abuse this bug!",
+                               "Staff Application": "Requirements:\n1. Req 1\n2. Req 2\n3. Req3"}
+        selected_description = ticket_descriptions.get(selection, "")
+        # Create the overwrites
+        staff_team = discord.utils.get(interaction.guild.roles, name="Staff Team")
+        channel_overwrites = {interaction.user: discord.PermissionOverwrite(view_channel=True, send_messages=False),
+                              staff_team: discord.PermissionOverwrite(view_channel=True, send_messages=True),
+                              interaction.guild.default_role: discord.PermissionOverwrite(view_channel=False)}
+        # Make the channel
+        number = "0001"
+        category = discord.utils.get(interaction.guild.channels, name=selection)
+        channel = await interaction.guild.create_text_channel(name=f"{interaction.user.name}-{number}", overwrites=channel_overwrites, category=category)
+        # Send/Delete tag 
+        tag = await channel.send(staff_team.mention)
+        await tag.delete()
+        # Respond to the user
+        await interaction.followup.send(content=f"Successfully created your ticket! {channel.mention}", ephemeral=True)
+        # Send the ticket message in the ticket
+        embed = discord.Embed(description=f"Hey {interaction.user.mention}!\n \n"
+                                          ""
+                                          f"You have created a new ticket\n"
+                                          f"**Type:** {selection}\n \n"
+                                          ""
+                                          f"{selected_description}\n", color=discord.Color.greyple(), timestamp=datetime.datetime.utcnow())
+        await channel.send(embed=embed, view=EnterInfo(str(selection)))
 
-  @discord.ui.select(options= [discord.SelectOption(label="General Support", emoji="🎟️"),
-                               discord.SelectOption(label="Bug Support", emoji="🐛"),
-                               discord.SelectOption(label="Staff Application", emoji="🗒️")], placeholder="Select a ticket category...", custom_id="dropdown")
-  async def selectmenu(self, interaction: discord.Interaction, select: select):
-    await interaction.response.defer()
-    await interaction.message.edit(view=self)
-    await self.create_ticket(interaction, select.values[0])
-
-class Empty(discord.ui.View):
-  def __init__(self) -> None:
-    super().__init__(timeout=None)
+    @discord.ui.select(options= [discord.SelectOption(label="General Support", emoji="🎟️"),
+                                 discord.SelectOption(label="Bug Support", emoji="🐛"),
+                                 discord.SelectOption(label="Staff Application", emoji="🗒️")], 
+                        placeholder="Select a ticket category...", 
+                        custom_id="dropdown")
+    async def selectmenu(self, interaction: discord.Interaction, select: ui.select):
+        await interaction.response.defer()
+        await interaction.message.edit(view=DropDown())
+        await self.create_ticket(interaction, select.values[0])
 
 class EnterInfo(discord.ui.View):
-  def __init__(self, ticket_type) -> None:
-    super().__init__(timeout=None)
-    self.ticket_type = ticket_type
+    def __init__(self, ticket_type: str) -> None:
+        self.ticket_type = ticket_type
+        super().__init__(timeout=None)
   
-  @discord.ui.button(label="Enter Information", style=discord.ButtonStyle.grey, custom_id="enter_information_button")
-  async def enterInfoButton(self, interaction: discord.Interaction, Button: discord.ui.Button):
-    await interaction.response.send_modal(PopUp(str(self.ticket_type)))
+    @discord.ui.button(label="Enter Information", style=discord.ButtonStyle.grey, custom_id="enter_information_button")
+    async def enterInfoButton(self, interaction: discord.Interaction, Button: discord.ui.Button):
+        await interaction.response.send_modal(PopUp(self.ticket_type))
 
 class PopUp(ui.Modal):
-  def __init__(self, ticket_type) -> None:
-    self.ticket_type = ticket_type
-    self.custom_id = "popup_modal"
-    super().__init__(title=self.ticket_type, timeout=None)
-    if self.ticket_type=="General Support":
-      self.add_item(ui.TextInput(label="What is your name?", placeholder="My name is...", custom_id="name_field_1"))
-      self.add_item(ui.TextInput(label="What is your problem?", placeholder="My problem is...", style=discord.TextStyle.long, custom_id="problem_field"))
-      self.add_item(ui.TextInput(label="When did this issue start?", placeholder="My name is...", custom_id="start_field"))
-    elif self.ticket_type=="Bug Report":
-      self.add_item(ui.TextInput(label="What is your name?", placeholder="My name is...", custom_id="name_field_2"))
-      self.add_item(ui.TextInput(label="Please explain the bug fully", placeholder="This is the bug...", style=discord.TextStyle.long, custom_id="explain_field"))
-      self.add_item(ui.TextInput(label="Video evidence of the bug", placeholder="https://youtube.com...", custom_id="video_field"))
-    elif self.ticket_type=="Staff Application":
-      self.add_item(ui.TextInput(label="What is your name?", placeholder="My name is...", custom_id="name_field_3"))
-      self.add_item(ui.TextInput(label="What role are you applying for?", placeholder="I am applying for this role...", style=discord.TextStyle.long, custom_id="role_field"))
-      self.add_item(ui.TextInput(label="How do you stand out?", placeholder="I stand out because...", custom_id="stand_out_field"))
+    def __init__(self, ticket_type: str) -> None:
+        self.ticket_type = ticket_type
+        super().__init__(timeout=None, title=self.ticket_type, custom_id="Modal")
 
-  async def on_submit(self, interaction: Interaction) -> None:
-    await interaction.response.defer()
+        self.add_item(ui.TextInput(label="What is your name?", placeholder="My name is...", style=discord.TextStyle.short))
+        if self.ticket_type == "General Support":
+            self.add_item(ui.TextInput(label="What is your issue?", placeholder="My issue is...", style=discord.TextStyle.long))
+            self.add_item(ui.TextInput(label="When did you first have this issue?", placeholder="I've been having this issue since...", style=discord.TextStyle.short))
+        
+        elif self.ticket_type == "Bug Support":
+            self.add_item(ui.TextInput(label="Please explain the bug in detail...", placeholder="Include video evidence...", style=discord.TextStyle.long))
+        
+        elif self.ticket_type == "Staff Application":
+            self.add_item(ui.TextInput(label="What are your past experiences?", placeholder="My experiences are...", style=discord.TextStyle.short))
 
-    embed = interaction.message.embeds[0]
-    split_description = embed.description.split("\n \n")
-    new_description = f"{split_description[0]}\n \n{split_description[1]}\n \n"
+    async def on_submit(self, interaction: discord.Interaction) -> None:
+        await interaction.response.defer()
+        # Place information in embed
+        embed = interaction.message.embeds[0]
+        description = embed.description
+        
+        split_description = description.split("\n \n")
 
-    for item in self.children:
-      new_description += f"**{item.label}** \n{item.value}\n \n"
-    
-    del split_description[0]
-    del split_description[0]
+        for item in reversed(self.children):
+            split_description.insert(2, f"**{item.label}**\n{item.value}")
+        
+        embed.description = "\n \n".join(split_description)
 
-    new_description += "\n \n".join(split_description)
-
-    embed.description = new_description
-    await interaction.message.edit(embed=embed, view=Empty())
-
-    permissions = interaction.channel.overwrites_for(interaction.user)
-    permissions.send_messages = True
-    await interaction.channel.set_permissions(interaction.user, overwrite=permissions)
+        await interaction.message.edit(view=None, embed=embed)
+        # Edit the channel permissions (allow member to send msgs)
+        permissions = interaction.channel.overwrites_for(interaction.user)
+        permissions.send_messages = True
+        await interaction.channel.set_permissions(target=interaction.user, overwrite=permissions)
 
 async def setup(client:commands.Bot) -> None:
   await client.add_cog(tickets(client))
